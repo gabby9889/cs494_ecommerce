100a593b247132e8718a425ed2ecf20eaa6ef8b7
msitu22 <msitu291@student.sfbu.edu>
2024-02-05 16:43:10 -0800

    section 4 update

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 import express from "express";
-import dotenv from "dotenv";
-
 const app = express();
+import dotenv from "dotenv";
+import productRoutes from "./routes/products.js";
+import { connectDatabase } from "./config/dbConnect.js";
+
+
 dotenv.config({ path: 'backend/config/config.env'});
 
+connectDatabase();
+
+app.use("/api/v1", productRoutes);
+
 app.listen(process.env.PORT, () => {
-    console.log(`Server listening on PORT: ${process.env.PORT}`);
+    console.log(
+        `Server listening on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+    );
 });
+
---
+++ backend/routes/products.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { getProducts } from "../controllers/productControllers.js";
+
+const router = express.Router();
+router.route("/products").get(getProducts);
+
+export default router;
