8752a9506cb4b32f1b4fc6ca833ed174d813ef62
gabby9889 <yying641@student.sfbu.edu>
2024-02-05 18:48:10 -0800

    edit routes by adding more functions of products

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { getProducts } from "../controllers/productControllers.js";
-
+import { deleteProduct, getProductDetails, getProducts, newProduct, updateProduct } from '../controllers/productController.js'
 const router = express.Router();
+
+
 router.route("/products").get(getProducts);
+router.route("/admin/products").post(newProduct);
+
+router.route("/products/:id").get(getProductDetails);
+router.route("/products/:id").put(updateProduct);
+router.route("/products/:id").delete(deleteProduct);
 
 export default router;
