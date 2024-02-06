0128eae1273045f6c2092641983bd00eb563a04e
gabby9889 <yying641@student.sfbu.edu>
2024-02-06 15:14:50 -0800

    add seeder in package

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,19 @@
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
-import productRoutes from "./routes/products.js";
+//import productRoutes from "./routes/products.js";
 import { connectDatabase } from "./config/dbConnect.js";
 
 
-dotenv.config({ path: 'backend/config/config.env'});
+dotenv.config({ path: 'backend/config/config.env' });
 
+// connectiong to database
 connectDatabase();
+
+//import all routes
+app.use(express.json());
+
+import productRoutes from "./routes/products.js";
 
 app.use("/api/v1", productRoutes);
 
