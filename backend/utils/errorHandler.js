5631dd670750b6cf6d919908ee004bde71c13fbf
Cassie Guo <cassieguo441@gmail.com>
2024-02-12 14:51:05 -0800

    create generic error handler

---
+++ backend/utils/errorHandler.js
@@ -0,0 +1,13 @@
+
+// Inherit from Error class
+class ErrorHandler extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    
+        // Create stack property
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+export default ErrorHandler;
