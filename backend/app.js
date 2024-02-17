18c90638c3e9218a56f7dce3fb58d797540dfb29
Cassie Guo <cassieguo441@gmail.com>
2024-02-17 15:13:24 -0800

    add error handlers for unhandled Promise Rejections & uncaught exceptions

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,12 @@ import dotenv from "dotenv";
 import { connectDatabase } from "./config/dbConnect.js";
 import errorMiddleware from "./middlewares/errors.js";
 
+// Handle Uncaught exceptions
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err}`);
+    console.log("Shutting down due to uncaught exception");
+    process.exit(1);
+});
 
 dotenv.config({ path: 'backend/config/config.env' });
 
@@ -20,9 +26,18 @@ app.use("/api/v1", productRoutes);
 // Using error middleware
 app.use(errorMiddleware);
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(
         `Server listening on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
     );
 });
 
+
+// Handle Unhandled Promise rejections 
+process.on('unhandledRejection', (err) => {
+    console.log(`ERROR: ${err}`);
+    console.logg("Shutting down server due to Unhandled Promise Rejection");
+    server.close(() => {
+        process.exit(1);
+    });
+});
