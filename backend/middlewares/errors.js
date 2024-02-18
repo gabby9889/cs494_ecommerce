1733493ff28abc987ec935b94068e58fa4cb429e
Cassie Guo <cassieguo441@gmail.com>
2024-02-17 16:54:40 -0800

    add Invalid Product ID handler & Validation Error handler

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -1,9 +1,25 @@
+import ErrorHandler from "../utils/errorHandler.js";
 
 export default (err, req, res, next) => {
     let error = {
         statusCode: err?.statusCode || 500,
         message: err?.message || "Internal Server Error",
     };
+
+    // Handler Invalid Mongoose ID Error
+    if(err.name === "CastError") {
+        const message = `Resouce not fund. Invalid: ${err?.path}`;
+        error = new ErrorHandler(message, 404);
+    }
+
+    // Handler Validation Error
+    if(err.name === "ValidationError") {
+        // .map() to show all errors, if applicable
+        const message = Object.values(err.errors).map((value) => value.message);
+        error = new ErrorHandler(message, 400);
+    }
+
+
 
     if (process.env.NODE_ENV === "DEVELOPMENT") {
         res.status(error.statusCode).json( {
