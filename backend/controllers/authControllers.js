e68bc2effa7548a51d7c5e0bf25a65ae780491e6
leemgzu <92341784+leemgzu@users.noreply.github.com>
2024-02-20 12:24:38 -0800

    Upload Section 9
    Finished section 9 and uploaded. Set up the environment before npm run dev. You need to run these first:
    npm i bcryptjs --save
    npm i jsonwebtoken --save
    npm i cookie-parser --save
    npm i nodemailer --save

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -129,3 +129,93 @@ export const resetPassword = catchAsyncErrors(async (req, res, next) => {
     sendToken(user, 200, res);
 });
 
+
+//Get current user profile => /api/v1/me
+export const getUserProfile = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req?.user?._id);
+
+    res.status(200).json({
+        user,
+    });
+});
+
+//Update Password => /api/v1/password/update
+export const updatePassword = catchAsyncErrors(async (req, res, next) => {
+    // check the old password 
+    const user = await User.findById(req?.user?._id).select("+password");
+    const isPasswordMatched = await user.comparePassword(req.body.oldPassword)
+    //update new password
+    if(!isPasswordMatched) {
+        return next(new ErrorHandler('Old Password is incorrect', 400));
+    }
+
+    user.password = req.body.password;
+    user.save();
+    res.status(200).json({
+        success: true,
+    });
+});
+
+//Update user profile => /api/v1/me/update
+export const updateProfile = catchAsyncErrors(async (req, res, next) => {
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email
+    };
+
+    const user = await User.findByIdAndUpdate(req.user._id, newUserData, {new:true});
+    res.status(200).json({
+        user,
+    });
+});
+
+//Get all users - ADMIN => /api/v1/admin/users
+export const allUsers = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.find();
+
+    res.status(200).json({
+        user,
+    });
+});
+
+//Get user details - ADMIN => /api/v1/admin/users/:id
+export const getUserDetails = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+    
+    if(!user) {
+        return next(new ErrorHandler(`User not found with id ${req.params.id}`, 404));
+    }
+    res.status(200).json({
+        user,
+    });
+});
+
+//Update user Details - ADMIN => /api/v1/admin/users/:id
+export const updateUser = catchAsyncErrors(async (req, res, next) => {
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+        role: req.body.role,
+    };
+    console.log(req.user._id);
+    const user = await User.findByIdAndUpdate(req.params.id, newUserData, {new:true});
+
+    res.status(200).json({
+        user,
+    });
+});
+
+//Delete user - ADMIN => /api/v1/admin/users/:id
+export const deleteUser = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.params.id);
+    
+    if(!user) {
+        return next(new ErrorHandler(`User not found with id ${req.params.id}`, 404));
+    }
+    // TODO - Remove user avatar from cloud
+    await user.deleteOne();
+
+    res.status(200).json({
+        user,
+    });
+});
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,7 @@
 import express from "express";
-import {loginUser, logout, registerUser, forgotPassword, resetPassword} from "../controllers/authControllers.js";
+import {loginUser, logout, registerUser, forgotPassword, resetPassword, getUserProfile, updatePassword, updateProfile, allUsers, getUserDetails, updateUser, deleteUser} from "../controllers/authControllers.js";
+import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
+import { getProductDetails } from "../controllers/productController.js";
 const router = express.Router();
 
 
@@ -9,5 +11,18 @@ router.route("/logout").get(logout);
 
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
+
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+
+router
+.route("/admin/users")
+.get(isAuthenticatedUser, authorizeRoles("admin"), allUsers);
+router
+.route("/admin/users/:id")
+.get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails)
+.put(isAuthenticatedUser, authorizeRoles("admin"), updateUser)
+.delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 export default router; 
