18d02d947be9f6d67e38fcfa04536ec910a50c46
Cassie Guo <cassieguo441@gmail.com>
2024-03-20 18:24:35 -0700

    calculate cart total

--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -54,73 +54,81 @@ const Cart = () => {
                 <>
                     <h2 className="mt-5">Your Cart: <b>{cartItems?.length} items</b></h2>
 
-                    <div className="row d-flex justify-content-between">
-                    <div className="col-12 col-lg-8">
+                        <div className="row d-flex justify-content-between">
+                            <div className="col-12 col-lg-8">
 
-                        {cartItems?.map((item) => (
-                            <>
-                            <hr />
-                            <div className="cart-item" data-key="product1">
-                            <div className="row">
-                            <div className="col-4 col-lg-3">
-                            <img
-                                src={item?.image}
-                                alt="Laptop"
-                                height="90"
-                                width="115"
-                            />
-                            </div>
-                            <div className="col-5 col-lg-3">
-                            <Link to={`/product/${item?.product}`}> 
-                                {" "}
-                                {item?.name}{" "} 
-                            </Link>
-                            </div>
-                            <div className="col-4 col-lg-2 mt-4 mt-lg-0">
-                            <p id="card_item_price">{item?.price}</p>
-                            </div>
-                            <div className="col-4 col-lg-3 mt-4 mt-lg-0">
-                            <div className="stockCounter d-inline">
-                                <span className="btn btn-danger minus" onClick={() => decreaseQty(item, item.quantity)}> - </span>
-                                <input
-                                type="number"
-                                className="form-control count d-inline"
-                                value={item?.quantity}
-                                readOnly
-                                />
-                                <span className="btn btn-primary plus" onClick={() => increaseQty(item, item.quantity)}> + </span>
-                            </div>
-                            </div>
-                            <div className="col-4 col-lg-1 mt-4 mt-lg-0">
-                            <i 
-                                id="delete_cart_item" 
-                                className="fa fa-trash btn btn-danger"
-                                onClick={() => removeCartItemHandler(item?.product)}
-                                ></i>
+                                {cartItems?.map((item) => (
+                                    <>
+                                    <hr />
+                                    <div className="cart-item" data-key="product1">
+                                    <div className="row">
+                                    <div className="col-4 col-lg-3">
+                                    <img
+                                        src={item?.image}
+                                        alt="Laptop"
+                                        height="90"
+                                        width="115"
+                                    />
+                                    </div>
+                                    <div className="col-5 col-lg-3">
+                                    <Link to={`/product/${item?.product}`}> 
+                                        {" "}
+                                        {item?.name}{" "} 
+                                    </Link>
+                                    </div>
+                                    <div className="col-4 col-lg-2 mt-4 mt-lg-0">
+                                    <p id="card_item_price">{item?.price}</p>
+                                    </div>
+                                    <div className="col-4 col-lg-3 mt-4 mt-lg-0">
+                                    <div className="stockCounter d-inline">
+                                        <span className="btn btn-danger minus" onClick={() => decreaseQty(item, item.quantity)}> - </span>
+                                        <input
+                                        type="number"
+                                        className="form-control count d-inline"
+                                        value={item?.quantity}
+                                        readOnly
+                                        />
+                                        <span className="btn btn-primary plus" onClick={() => increaseQty(item, item.quantity)}> + </span>
+                                    </div>
+                                    </div>
+                                    <div className="col-4 col-lg-1 mt-4 mt-lg-0">
+                                    <i 
+                                        id="delete_cart_item" 
+                                        className="fa fa-trash btn btn-danger"
+                                        onClick={() => removeCartItemHandler(item?.product)}
+                                        ></i>
+                                 </div>
                             </div>
                         </div>
-                        </div>
-                        <hr />
-                            </>
+                    <hr />
+                </>
                         ))}
 
                         
 
 
                     </div>
-
-                    <div className="col-12 col-lg-3 my-4">
-                        <div id="order_summary">
-                        <h4>Order Summary</h4>
-                        <hr />
-                        <p>Subtotal: <span className="order-summary-values">8 (Units)</span></p>
-                        <p>Est. total: <span className="order-summary-values">$1499.97</span></p>
-                        <hr />
-                        <button id="checkout_btn" className="btn btn-primary w-100">
-                            Check out
-                        </button>
+                        <div className="col-12 col-lg-3 my-4">
+                            <div id="order_summary">
+                            <h4>Order Summary</h4>
+                            <hr />
+                            <p>Subtotal: 
+                                <span className="order-summary-values">
+                                    {cartItems?.reduce((acc, item) => acc + item?.quantity, 0)}
+                                    (Units)
+                                </span>
+                            </p>
+                            <p>Est. total: 
+                                <span className="order-summary-values">
+                                    ${cartItems?.reduce((acc, item) => acc + item?.quantity * item?.price, 0).toFixed(2)}
+                                </span>
+                            </p>
+                            <hr />
+                            <button id="checkout_btn" className="btn btn-primary w-100">
+                                Check out
+                            </button>
+                            </div>
                         </div>
-                    </div>
                     </div>
                 </>
             )
